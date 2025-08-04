43634697de1d95ca51fe0971e7261e4ce43bb5fe
Saiteja2580 <saitejassp2580@gmail.com>
2025-08-04 22:47:17 +0530

    backend url updated

--- assistant-client/src/env/env.local.ts
+++ assistant-client/src/env/env.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'https://ai-student-cip.onrender.com/api',
   auth: {
     domain: 'dev-zizcs5o7lxt4w3ao.us.auth0.com',
     clientId: 'rq7zfDLw9F4b5Ky93tpJRK0ZhmEiQyXF',
